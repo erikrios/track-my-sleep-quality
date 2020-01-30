be6734a6c54fbc7055e5339f237d7e770baa659a
erikrios <erikriosetiawan49@gmail.com>
2020-01-30 19:27:59 +0700

    Implement the data binding

--- app/src/main/java/com/erikriosetiawan/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/erikriosetiawan/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -2,12 +2,13 @@ package com.erikriosetiawan.trackmysleepquality.sleeptracker
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.erikriosetiawan.trackmysleepquality.R
+import com.erikriosetiawan.trackmysleepquality.databinding.FragmentSleepTrackerBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,8 +19,14 @@ class SleepTrackerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sleep_tracker, container, false)
+        val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_sleep_tracker,
+            container,
+            false
+        )
+
+        return binding.root
     }
 
 
