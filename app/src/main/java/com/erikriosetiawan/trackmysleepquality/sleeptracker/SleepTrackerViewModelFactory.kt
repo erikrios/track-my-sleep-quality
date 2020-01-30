b8d2fb06bab28b18b921a3432c2670a14a1be9c7
erikrios <erikriosetiawan49@gmail.com>
2020-01-30 19:38:23 +0700

    Add a view model factory for the fragment

---
+++ app/src/main/java/com/erikriosetiawan/trackmysleepquality/sleeptracker/SleepTrackerViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.erikriosetiawan.trackmysleepquality.sleeptracker
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.erikriosetiawan.trackmysleepquality.database.SleepDatabaseDao
+
+class SleepTrackerViewModelFactory(
+    private val dataSource: SleepDatabaseDao,
+    private val application: Application
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(
+                SleepTrackerViewModel::class.java
+            )
+        ) {
+            return SleepTrackerViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
