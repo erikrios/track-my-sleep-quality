63613f6555437ff1d2e5ac44ea2428a1afc2574d
erikrios <erikriosetiawan49@gmail.com>
2020-01-30 19:31:07 +0700

    Implement the data binding

--- app/src/main/java/com/erikriosetiawan/trackmysleepquality/sleepquality/SleepQualityFragment.kt
+++ app/src/main/java/com/erikriosetiawan/trackmysleepquality/sleepquality/SleepQualityFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.erikriosetiawan.trackmysleepquality.R
+import com.erikriosetiawan.trackmysleepquality.databinding.FragmentSleepQualityBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,8 +20,14 @@ class SleepQualityFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sleep_quality, container, false)
+        val binding: FragmentSleepQualityBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_sleep_quality, container, false
+        )
+
+        val application = requireNotNull(this.activity).application
+
+        return binding.root
     }
 
 
