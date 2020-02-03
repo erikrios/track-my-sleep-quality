48af6e036ab82019ec69bb96071f7b8a3c4eddb2
erikrios <erikriosetiawan49@gmail.com>
2020-02-03 19:22:09 +0700

    Create a ViewModel and ViewModelFactory

--- app/src/main/java/com/erikriosetiawan/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/erikriosetiawan/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -1,2 +1,44 @@
 package com.erikriosetiawan.trackmysleepquality.sleepquality
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.erikriosetiawan.trackmysleepquality.database.SleepDatabaseDao
+import kotlinx.coroutines.*
+
+class SleepQualityViewModel(
+    private val sleepNightkey: Long = 0L,
+    val database: SleepDatabaseDao
+) : ViewModel() {
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    private val viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    fun doneNavigating() {
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onSetSleepQuality(quality: Int) {
+        uiScope.launch {
+            // IO is a thread pool for running operations that access the disk, such as
+            // our Room database.
+            withContext(Dispatchers.IO) {
+                val tonight = database.get(sleepNightkey) ?: return@withContext
+                tonight.sleepQuality = quality
+                database.update(tonight)
+            }
+
+            // Setting this state variable to true will alert the observer and trigger navigation.
+            _navigateToSleepTracker.value = true
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
--- app/src/main/java/com/erikriosetiawan/trackmysleepquality/sleepquality/SleepQualityViewModelFactory.kt
+++ app/src/main/java/com/erikriosetiawan/trackmysleepquality/sleepquality/SleepQualityViewModelFactory.kt
@@ -1,2 +1,19 @@
 package com.erikriosetiawan.trackmysleepquality.sleepquality
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.erikriosetiawan.trackmysleepquality.database.SleepDatabaseDao
+
+@Suppress("UNCHECKED_CAST")
+class SleepQualityViewModelFactory(
+    private val sleepNight: Long,
+    private val dataSource: SleepDatabaseDao
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepQualityViewModel::class.java)) {
+            return SleepQualityViewModel(sleepNight, dataSource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
